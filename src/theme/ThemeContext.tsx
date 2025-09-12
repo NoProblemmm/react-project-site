62f1e8501b7c2aa190e3959d1ba02ad84fe73a0f
noProblem <StiLinius2255@yandex.ru>
2025-09-12 22:16:08 +0300

    theme & tailwind

---
+++ src/theme/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext({
+  theme: "light",
+  switchTheme: () => {},
+});
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState(getInitialTheme());
+
+  function getInitialTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) return savedTheme;
+
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (savedTheme) return savedTheme;
+    const hours = new Date().getHours();
+    return hours < 6 || hours >= 22 ? "dark" : "light";
+  }
+
+  const switchTheme = () => {
+    setTheme((prev) => {
+      const newTheme = prev === "light" ? "dark" : "light";
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+  useEffect(() => {
+    document.documentElement.className = theme === "dark" ? "dark" : "light";
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, switchTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => useContext(ThemeContext);
