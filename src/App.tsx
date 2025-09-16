81866d537794f41618e37872436e92562c0bc9e1
noProblem <StiLinius2255@yandex.ru>
2025-09-16 21:35:10 +0300

    add lingui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import "./theme/typeTheme/theme.css";
+import { i18n } from "@lingui/core";
+import { messages as ruMassages } from "./locales/ru/messages";
+import { messages as enMassages } from "./locales/en/messages";
 
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
@@ -9,7 +12,19 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
+
 function App() {
+  // Использование lingui
+  useEffect(() => {
+    const saveLocal = localStorage.getItem("language") || "en";
+    if (saveLocal === "ru") {
+      i18n.load("ru", ruMassages);
+      i18n.activate("ru");
+    } else {
+      i18n.load("en", enMassages);
+      i18n.activate("en");
+    }
+  });
   return (
     <>
       {/* // <ThemeProvider> */}
