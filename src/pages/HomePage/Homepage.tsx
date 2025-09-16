6ea21c16d32272b8bd12b3f967c1e710ecc48126
noProblem <StiLinius2255@yandex.ru>
2025-09-16 21:40:29 +0300

    add lingui

--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Layout, Spin } from "antd";
 import { observer } from "mobx-react-lite";
+import { I18nProvider } from "@lingui/react";
+import { i18n } from "@lingui/core";
 
 // @ts-ignore
 import { AppHeader } from "@components/Layout/AppHeader";
@@ -23,26 +25,32 @@ function HomePage() {
 
   return (
     <>
-      <title>HomePage</title>
+      <I18nProvider i18n={i18n}>
+        <title>HomePage</title>
 
-      <link rel="icon" type="image" href="/static/favicon.ico" />
-      {!isLoading ? (
-        <Layout>
-          <AppHeader showButtons={true} />
+        <link rel="icon" type="image" href="/static/favicon.ico" />
+        {!isLoading ? (
           <Layout>
-            <AppSider />
-            <AppContent />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <AppHeader showButtons={true} />
+            <Layout>
+              <AppSider />
+              <AppContent />
+            </Layout>
+            <AppFooter />
           </Layout>
-          <AppFooter />
-        </Layout>
-      ) : (
-        <div style={{ width: "100%", height: "100%" }}>
-          <Spin
-            size="large"
-            style={{ marginTop: "20%", width: "100%", height: "500px" }}
-          />
-        </div>
-      )}
+        ) : (
+          <div style={{ width: "100%", height: "100%" }}>
+            <Spin
+              size="large"
+              style={{ marginTop: "20%", width: "100%", height: "500px" }}
+            />
+          </div>
+        )}
+      </I18nProvider>
     </>
   );
 }
