d69e778427f9bb31968034a262a292686580919b
noProblem <StiLinius2255@yandex.ru>
2025-09-17 20:45:55 +0300

    add timer

--- src/components/Layout/AppFooter.tsx
+++ src/components/Layout/AppFooter.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout, Typography } from "antd";
 import { useTheme } from "../../theme/SwitchTheme";
 const { Footer } = Layout;
 
 export function AppFooter() {
   const { theme } = useTheme();
+  let [timer, setTimer] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <Footer className="footer-custom ">
       <Typography className="footer-custom text-center">TaskBook</Typography>
+      <Typography className="footer-custom text-center">
+        Текущее время: {timer.toLocaleTimeString()}
+      </Typography>
     </Footer>
   );
 }
