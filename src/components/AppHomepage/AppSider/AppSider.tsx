c0d77b2198e00dccec61959154b15575a7f43874
noProblem <StiLinius2255@yandex.ru>
2025-09-18 15:41:06 +0300

    add search card book

--- src/components/AppHomepage/AppSider/AppSider.tsx
+++ src/components/AppHomepage/AppSider/AppSider.tsx
@@ -5,6 +5,7 @@ import { taskBookStore } from "../../../store/taskbook.store";
 import { observer } from "mobx-react-lite";
 import { useTheme } from "../../../theme/SwitchTheme";
 import { useTranslation } from "react-i18next";
+import { SearchInput } from "../../ui/SearchInput/SearchInput";
 const { Sider } = Layout;
 
 export const AppSider = observer(() => {
@@ -12,10 +13,35 @@ export const AppSider = observer(() => {
   const { deleteBook, taskBooks, selectBook } = taskBookStore;
   const { theme } = useTheme();
 
+  const [searchValue, setSearchValue] = useState<String>("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const searchCardBook = () => {
+    if (searchValue.trim() === "") {
+      return taskBooks;
+    } else {
+      return taskBooks.filter((book) =>
+        book.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+      );
+    }
+  };
+  const taskBook = searchCardBook();
+
   return (
-    <Sider width="25%" className="sider-custom h-screen ">
-      <div className="scrollbar-sider w-[100%] overflow-x-hidden h-screen overflow-y-scroll ml-[2px]">
-        {taskBooks.map((book) => (
+    <Sider width="25%" className="sider-custom h-[100%] ">
+      <div className="m-1">
+        <SearchInput
+          value={searchValue}
+          setSearchValue={setSearchValue}
+          onChange={handleSearchChange}
+        />
+      </div>
+
+      <div className="scrollbar-sider w-[99%] overflow-x-hidden h-screen overflow-y-scroll ml-[2px]">
+        {taskBook.map((book) => (
           <Card
             key={book.id}
             title={book.title}
---
+++ src/components/ui/SearchInput/SearchInput.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from "react";
+import "./SearchInput.css";
+
+type Props = {
+  value: any;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  setSearchValue: (value: string) => void;
+};
+
+export function SearchInput({ value, onChange, setSearchValue }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleInpurActive = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const deleteInputValue = () => {
+    setSearchValue("");
+  };
+
+  return (
+    <div className="container">
+      <img
+        className="img-search"
+        onClick={handleInpurActive}
+        src="/static/search.svg"
+      ></img>
+
+      <input
+        ref={inputRef}
+        placeholder="Search ..."
+        className="search-input"
+        value={value}
+        onChange={onChange}
+        type="text"
+      />
+      {value && (
+        <img
+          className="img-close"
+          src="/static/close.svg"
+          onClick={deleteInputValue}
+        ></img>
+      )}
+    </div>
+  );
+}
