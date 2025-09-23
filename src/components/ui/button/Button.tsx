8fb55c8de112de002f5310446ed693ce3f231b7a
noProblem <StiLinius2255@yandex.ru>
2025-09-23 22:46:49 +0300

    add ui button

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,15 @@
+import {
+  ComponentProps,
+  forwardRef,
+  ElementRef,
+  PropsWithChildren,
+  memo,
+  FC,
+} from "react";
+import { Button as ButtonAnt } from "antd";
+
+type ButtonProps = ComponentProps<typeof ButtonAnt>;
+
+export const Button: FC<PropsWithChildren<ButtonProps>> = memo((props) => {
+  return <ButtonAnt {...props} />;
+});
